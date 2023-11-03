a448b32533344bbc6d92f2495db07e0f70958edf
Abeto <abetol2001@gmail.com>
2023-11-03 12:08:00 +0100

    comportamientos añadidos

--- Practica2/src/Agent203.java
+++ Practica2/src/Agent203.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 import jade.core.Agent;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -12,6 +14,8 @@ public class Agent203 extends Agent{
     
     // Entorno
     private Environment environment;
+    private Direction nextDirection;
+    private List<Point2D> agentPath;
     
     // Memoria del agente
     // ...
@@ -19,11 +23,13 @@ public class Agent203 extends Agent{
     @Override
     protected void setup() {
         // Inicializar aquí el entorno
-        String path = "./maps/mapWithoutObstacle.txt";
+        String mapPath = "./maps/mapWithoutObstacle.txt";
         Point2D initialPosition = new Point2D(0, 0);
         Point2D goalPosition = new Point2D(2, 2);
+        
+        agentPath = new LinkedList<>();
 
-        this.environment = new Environment(path, initialPosition, goalPosition);
+        this.environment = new Environment(mapPath, initialPosition, goalPosition);
 
         
         // Definir comportamientos
