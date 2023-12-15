c1042903d4f28e1cb83c161d1946db2694637f36
Carlos QP <e.carlosqp@go.ugr.es>
2023-12-15 11:45:25 +0100

    deleted unused files

---
+++ Practica2/src/core/Globals.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ * @brief Almacena las constantes globales de comunicación
+ * @author carlosqp
+ */
+public class Globals {
+
+    public static final String SECRET_CODE = "your_secret_code";
+    public static final String SANTA_AID = "santa_aid";
+    public static final String AGENT203_AID = "agent203_aid";
+    public static final String RUDOLF_AID = "rudolph_aid";
+    
+}
